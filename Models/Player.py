88a9c602ef86630be35587a3efa3d50ca58c0094
8astien <bastien.cauquil@pm.me>
2023-05-29 15:41:39 +0200

    Reorganisation du code

--- Models/Player.py
+++ Models/Player.py
@@ -1,7 +1,23 @@
+import json
+import os
+
+
 class Player:
-    def __init__(self, first_name, last_name, birth_date, national_chess_id, id):
+    def __init__(self, first_name, last_name, birth_date, national_chess_id):
         self.first_name = first_name
         self.last_name = last_name
         self.birth_date = birth_date
         self.national_chess_id = national_chess_id
-        self.id = id
+
+    def add_player(self):
+        new_player = self.__dict__
+        if not os.path.exists('players.json'):
+            with open('players.json', 'w') as file:
+                json.dump([new_player], file, indent=4)
+        else:
+            with open('players.json', 'r+') as file:
+                players = json.load(file)
+                players.append(new_player)
+                file.seek(0)
+                file.truncate()
+                json.dump(players, file, indent=4)
--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-from Models.Player import Player
-from Views.PlayerView import PlayerView
 from Controllers.PlayerController import PlayerController
+
 
 def main():
     while True:
+        playerController = PlayerController()
+
         # Demander à l'utilisateur s'il souhaite ajouter un joueur
         add_player = input("Souhaitez-vous ajouter un joueur ? (O/N): ")
 
         # Si l'utilisateur veut ajouter un joueur
         if add_player.lower() == 'o':
-            PlayerController.add_player()
+            playerController.add_player()
         else:
             break
+
 
 if __name__ == '__main__':
     main()
